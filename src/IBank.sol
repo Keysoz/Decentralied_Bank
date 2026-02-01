d6121b1d5381197475ebe80d0ce8776c2117342f
Keysoz <keysoz122@gmail.com>
2026-02-01 23:27:23 +0200

    New: Some Bank.sol Improvements

--- src/IBank.sol
+++ src/IBank.sol
@@ -1,18 +1,12 @@
 /**
- * @title: The Decentralized Bank
- * @author _Keysoz_
- * @notice **What To Expect**
- * - The System Does What a **traditional Bank** Does in a decentralized World.
- * - The Core Functions Are (Deposit, Withdraw, Transfer And Transfer Internally).
- * - The System is safe so no need to be worry.
- * @dev *The Security Concerns**
- * - The Modern Security Checks Are Done.
- * - Openzeppelin Modifiers (Ownable, Pausable) Are Used.
- * - The CEI (Check, Effects, Interactions) Are Used.
+ * @title Banking System
+ * @author Ahmed Ramadan
+ * @notice A Decentralized Banking System With The Core Functions of Any Traditional Bank=
+ * @dev The Security of each function is tested in depth
  */
 
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity >= 0.8.13;
 
 interface IBank {
     /* Errors */
@@ -31,53 +25,36 @@ interface IBank {
     event TransferOwnership(address indexed oldOwner, address indexed newOwner);
 
     /* Function Signatures */
-    /**
-     * @notice **Warning**: Don't Try To Deposit Zero Amount.
-     * @dev For Security: The Zero Amount Deposit Check is Done.
-     */
     function deposit() external payable;
     /**
-     * @dev *I Used the CEI (Check-Effects-Interactions) in the Withdraw functions.**
-     * - Check: check if user balance is not sufficient
-     * - Effects: Make The Changes in the contract level first.
-     * - Interactions: make `.call` to make the changes in the blockchain level
-     * @param amount The Amount You Want to Withdraw
+     * @param amount the amount to be transferred
+     * @notice This function to transfer external the bank
      */
     function withdraw(uint256 amount) external;
     /**
-     * @notice **Note**: This Function Send Eth To Any Address Even Outside The Bank System (e.g. External Wallet).
-     * @param amount The AMount You Want To Send.
-     * @param receiver The Address Of The Receiver.
+     * @param receiver the address you want to transfer to
+     * @param amount the amount to be transferred
+     * @notice This function to transfer external the bank
      */
     function transferTo(address receiver, uint256 amount) external;
     /**
-     * @notice **Note**: This Function Send Eth Between Accounts Inside The Bank System.
-     * @param amount The AMount You Want To Send.
-     * @param receiver The Address Of The Receiver.
+     * @param receiver the address you want to transfer to
+     * @param amount the amount to be transferred
+     * @notice This function to transfer internal the bank
      */
     function transferInternal(address receiver, uint256 amount) external;
     /**
-     * @notice **Warning**: Double Check `newOwner` Address.
-     * @dev *This a Very Dangerous Function So Be Careful**
-     * - It's a single step function.
-     * - 2-step function is under development.
-     * - double-check the address of the new owner for now.
+     * @param newOwner will be the new owner
+     * @notice the newOwner will control the contract
+     * @dev Warning: This is a single-step ownership transfer process
      */
-    function transferContractOwnership(address newOwner) external;
+    function transferOwnership(address newOwner) external;
     /**
-     * @dev *Note**: This Function can only be called in Emergency.
-     */
-    function pause() external;
-    /**
-     * @dev *Note**: This Function can only be called if The Danger Has gone.
-     */
-    function unpause() external;
-    /**
-     * @return The Total Balance of the Bank
+     * @return The Total Balance of the BanK
      */
     function getTotalBalance() external view returns (uint256);
     /**
-     * @param user The User Address That you want to know it's balance.
+     * @param user the user you want to know it's balance
      * @return The Total Balance of the user in the Bank
      */
     function getBalance(address user) external view returns (uint256);
