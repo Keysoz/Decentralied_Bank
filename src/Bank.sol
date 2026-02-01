03bb590b6271d31fcaeba1da254d7a607083dec2
Keysoz <keysoz122@gmail.com>
2026-02-01 23:31:53 +0200

    New: Some Bank.sol, IBank.sol, Bank.t.sol New Code

--- src/Bank.sol
+++ src/Bank.sol
@@ -1,16 +1,24 @@
 /**
- * @title Banking System
- * @author Ahmed Ramadan
- * @notice A Decentralized Banking System With The Core Functions of Any Traditional Bank=
- * @dev The Security of each function is tested in depth
+ * @title: The Decentralized Bank
+ * @author _Keysoz_
+ * @notice **What To Expect**
+ * - The System Does What a **traditional Bank** Does in a decentralized World.
+ * - The Core Functions Are (Deposit, Withdraw, Transfer And Transfer Internally).
+ * - The System is safe so no need to be worry.
+ * @dev *The Security Concerns**
+ * - The Modern Security Checks Are Done.
+ * - Openzeppelin Modifiers (Ownable, Pausable) Are Used.
+ * - The CEI (Check, Effects, Interactions) Are Used.
  */
 
 // SPDX-License-Identifier: MIT
-pragma solidity >= 0.8.13;
+pragma solidity ^0.8.20;
 
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
 import {IBank} from "./IBank.sol";
 
-contract Bank is IBank {
+contract Bank is IBank, Ownable, Pausable {
     /* State Variables Start */
     mapping(address => uint256) private s_addressToBalance;
     address private s_owner;
@@ -23,39 +31,40 @@ contract Bank is IBank {
         _;
     }
 
-    modifier onlyOwner() {
-        if (msg.sender != s_owner) revert Bank__NotTheOwner();
-        _;
-    }
     /* Modifiers End */
 
     /* Functions Start */
-    constructor() {
-        s_owner = msg.sender;
-    }
+    constructor() Ownable(msg.sender) {}
 
     receive() external payable {
         deposit();
     }
 
+    /// @inheritdoc IBank
     function deposit() public payable override {
-        if (msg.value == 0) revert Bank__UserCantDepositZeroAmount();
+        address depositor = msg.sender;
+        uint256 depositAmount = msg.value;
 
-        s_addressToBalance[msg.sender] += msg.value;
-        s_totalBankBalance += msg.value;
+        if (depositAmount == 0) revert Bank__UserCantDepositZeroAmount();
 
-        emit Deposit(msg.sender, msg.value);
+        s_addressToBalance[depositor] += depositAmount;
+        s_totalBankBalance += depositAmount;
+
+        emit Deposit(depositor, depositAmount);
     }
 
     /// @inheritdoc IBank
-    function withdraw(uint256 amount) public override {
+    function withdraw(uint256 amount) public override whenNotPaused {
+        address withdrawer = msg.sender;
+        uint256 withdrawerBalance = s_addressToBalance[msg.sender];
+
         if (amount == 0) revert Bank__UserCantWithdrawZeroAmount();
 
-        if (amount > s_addressToBalance[msg.sender]) {
-            revert Bank__InsufficientBalance(s_addressToBalance[msg.sender], amount);
+        if (amount > withdrawerBalance || withdrawerBalance == 0) {
+            revert Bank__InsufficientBalance(withdrawerBalance, amount);
         }
 
-        s_addressToBalance[msg.sender] -= amount;
+        s_addressToBalance[withdrawer] -= amount;
         s_totalBankBalance -= amount;
 
         (bool success,) = payable(msg.sender).call{value: amount}("");
@@ -65,47 +74,69 @@ contract Bank is IBank {
     }
 
     /// @inheritdoc IBank
-    function transferTo(address receiver, uint256 amount) public override checkZeroAddress(receiver) {
+    function transferTo(address receiver, uint256 amount) public override whenNotPaused checkZeroAddress(receiver) {
+        address caller = msg.sender;
+        uint256 callerBalance = s_addressToBalance[msg.sender];
+
         if (amount == 0) revert Bank__UserCantWithdrawZeroAmount();
 
-        uint256 callerBalance = s_addressToBalance[msg.sender];
-        if (amount > callerBalance) {
+        if (amount > callerBalance || callerBalance == 0) {
             revert Bank__InsufficientBalance(callerBalance, amount);
         }
 
-        s_addressToBalance[msg.sender] -= amount;
+        s_addressToBalance[caller] -= amount;
         s_totalBankBalance -= amount;
 
         (bool success,) = payable(receiver).call{value: amount}("");
         if (!success) revert Bank__TransferFailed();
 
-        emit TransferTo(msg.sender, receiver, amount);
+        emit TransferTo(caller, receiver, amount);
     }
 
     /// @inheritdoc IBank
-    function transferInternal(address receiver, uint256 amount) public override checkZeroAddress(receiver) {
+    function transferInternal(address receiver, uint256 amount)
+        public
+        override
+        whenNotPaused
+        checkZeroAddress(receiver)
+    {
+        address sender = msg.sender;
+        uint256 senderBalance = s_addressToBalance[msg.sender];
+
         if (amount == 0) revert Bank__UserCantWithdrawZeroAmount();
 
-        uint256 callerBalance = s_addressToBalance[msg.sender];
-        if (amount > callerBalance) {
-            revert Bank__InsufficientBalance(callerBalance, amount);
+        if (amount > senderBalance) {
+            revert Bank__InsufficientBalance(senderBalance, amount);
         }
 
-        s_addressToBalance[msg.sender] -= amount;
+        s_addressToBalance[sender] -= amount;
         s_addressToBalance[receiver] += amount;
 
-        emit TransferInternal(msg.sender, receiver, amount);
+        emit TransferInternal(sender, receiver, amount);
     }
 
     /// @inheritdoc IBank
-    function transferOwnership(address newOwner) public override onlyOwner {
+    function transferContractOwnership(address newOwner) public override onlyOwner {
+        address oldOwner = s_owner;
+
         if (newOwner == address(0)) revert Bank__InvalidAddress(newOwner);
-        address oldOWner = s_owner;
+
         s_owner = newOwner;
-        emit TransferOwnership(oldOWner, newOwner);
+
+        emit TransferOwnership(oldOwner, newOwner);
     }
 
     /// @inheritdoc IBank
+    function pause() external override onlyOwner {
+        _pause();
+    }
+
+    /// @inheritdoc IBank
+    function unpause() external override onlyOwner {
+        _unpause();
+    }
+    /// @inheritdoc IBank
+
     function getTotalBalance() public view override onlyOwner returns (uint256) {
         return address(this).balance;
     }
--- src/IBank.sol
+++ src/IBank.sol
@@ -1,12 +1,18 @@
 /**
- * @title Banking System
- * @author Ahmed Ramadan
- * @notice A Decentralized Banking System With The Core Functions of Any Traditional Bank=
- * @dev The Security of each function is tested in depth
+ * @title: The Decentralized Bank
+ * @author _Keysoz_
+ * @notice **What To Expect**
+ * - The System Does What a **traditional Bank** Does in a decentralized World.
+ * - The Core Functions Are (Deposit, Withdraw, Transfer And Transfer Internally).
+ * - The System is safe so no need to be worry.
+ * @dev *The Security Concerns**
+ * - The Modern Security Checks Are Done.
+ * - Openzeppelin Modifiers (Ownable, Pausable) Are Used.
+ * - The CEI (Check, Effects, Interactions) Are Used.
  */
 
 // SPDX-License-Identifier: MIT
-pragma solidity >= 0.8.13;
+pragma solidity ^0.8.20;
 
 interface IBank {
     /* Errors */
@@ -25,36 +31,53 @@ interface IBank {
     event TransferOwnership(address indexed oldOwner, address indexed newOwner);
 
     /* Function Signatures */
+    /**
+     * @notice **Warning**: Don't Try To Deposit Zero Amount.
+     * @dev For Security: The Zero Amount Deposit Check is Done.
+     */
     function deposit() external payable;
     /**
-     * @param amount the amount to be transferred
-     * @notice This function to transfer external the bank
+     * @dev *I Used the CEI (Check-Effects-Interactions) in the Withdraw functions.**
+     * - Check: check if user balance is not sufficient
+     * - Effects: Make The Changes in the contract level first.
+     * - Interactions: make `.call` to make the changes in the blockchain level
+     * @param amount The Amount You Want to Withdraw
      */
     function withdraw(uint256 amount) external;
     /**
-     * @param receiver the address you want to transfer to
-     * @param amount the amount to be transferred
-     * @notice This function to transfer external the bank
+     * @notice **Note**: This Function Send Eth To Any Address Even Outside The Bank System (e.g. External Wallet).
+     * @param amount The AMount You Want To Send.
+     * @param receiver The Address Of The Receiver.
      */
     function transferTo(address receiver, uint256 amount) external;
     /**
-     * @param receiver the address you want to transfer to
-     * @param amount the amount to be transferred
-     * @notice This function to transfer internal the bank
+     * @notice **Note**: This Function Send Eth Between Accounts Inside The Bank System.
+     * @param amount The AMount You Want To Send.
+     * @param receiver The Address Of The Receiver.
      */
     function transferInternal(address receiver, uint256 amount) external;
     /**
-     * @param newOwner will be the new owner
-     * @notice the newOwner will control the contract
-     * @dev Warning: This is a single-step ownership transfer process
+     * @notice **Warning**: Double Check `newOwner` Address.
+     * @dev *This a Very Dangerous Function So Be Careful**
+     * - It's a single step function.
+     * - 2-step function is under development.
+     * - double-check the address of the new owner for now.
      */
-    function transferOwnership(address newOwner) external;
+    function transferContractOwnership(address newOwner) external;
     /**
-     * @return The Total Balance of the BanK
+     * @dev *Note**: This Function can only be called in Emergency.
+     */
+    function pause() external;
+    /**
+     * @dev *Note**: This Function can only be called if The Danger Has gone.
+     */
+    function unpause() external;
+    /**
+     * @return The Total Balance of the Bank
      */
     function getTotalBalance() external view returns (uint256);
     /**
-     * @param user the user you want to know it's balance
+     * @param user The User Address That you want to know it's balance.
      * @return The Total Balance of the user in the Bank
      */
     function getBalance(address user) external view returns (uint256);
