e9ee1cd552714a90cd992b7777e3db1e683b3cdf
Keysoz <keysoz122@gmail.com>
2026-02-02 03:16:56 +0200

    New: Deployment Script File

--- script/DeployBank.s.sol
+++ script/DeployBank.s.sol
@@ -1,7 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-// import {Script} from "forge-std/Script.sol";
-// import {Bank} from "../src/Bank.sol";
+import {Script} from "forge-std/Script.sol";
+import {Bank} from "../src/Bank.sol";
 
-// contract DeployBank is Script {}
+contract DeployBank is Script {
+    function run() external returns (Bank) {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        vm.startBroadcast(deployerPrivateKey);
+        Bank bank = new Bank();
+        vm.stopBroadcast();
+
+        return bank;
+    }
+}
